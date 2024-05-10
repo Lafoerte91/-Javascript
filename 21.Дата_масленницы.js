231f080a1dd891d15eff127152280004f3431d5f
Lafoerte91 <lafoerte@gmail.com>
2024-05-10 10:24:22 +0300

    edit

--- 21.Дата_масленницы.js
+++ 21.Дата_масленницы.js
@@ -7,13 +7,12 @@ function getNextMaslenitsa() {
   const nextYear = currentYear + 1
 
   // Найти последнее воскресенье февраля (или марта, если февраль 29 дней)
-  const lastFebruarySunday = new Date(currentYear, 1, 29) // Начать с 28 февраля
+  const lastFebruarySunday = new Date(currentYear, 1, 28) // Начать с 28 февраля
   while(lastFebruarySunday.getDay() !== 0) {
     lastFebruarySunday.setDate(lastFebruarySunday.getDate() + 1)
   }
   // Если Масленица уже прошла в этом году, вернуть дату следующего года
   if(today > lastFebruarySunday) {
-    console.log(lastFebruarySunday.getDate())
     return new Date(nextYear, lastFebruarySunday.getMonth(), lastFebruarySunday.getDate())
   } else {
     return lastFebruarySunday
